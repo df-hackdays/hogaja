3422f2914e56fc450f4def406bac0c17f8e6c0f3
Jay <jay.hiranandani@scotiabank.com>
2018-09-29 14:26:59 -0400

    adding push notifications from firebase

---
+++ app/src/main/java/com/scotiabank/notifications/MyFirebaseInstanceIDService.java
@@ -0,0 +1,22 @@
+package com.scotiabank.notifications;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIIDService";
+    private static final String FirebaseToken = "FirebaseTokenKey";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        preferences.edit().putString(FirebaseToken, refreshedToken).apply();
+    }
+}
